8fc45db3ce2723303f6f6b73348a32cf23bef6c5
Rifat <147029256+Rifat392000@users.noreply.github.com>
2024-01-09 17:43:54 +0600

    Iteration

---
+++ 02.js
@@ -0,0 +1,30 @@
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+let score1 = 5
+
+do {
+    console.log(`Score is ${score1}`);
+    score1++
+} while (score1 <= 10);
+
+
+let score2 = 11
+
+do {
+    console.log(`Score is ${score2}`);
+    score2++
+} while (score2 <= 10);
